1170699f5b485d99928c28dad1ade638dee92966
Starling <nakboyloy50@gmail.com>
2026-06-24 15:33:51 +0700

    up

---
+++ Controller.java
@@ -0,0 +1,109 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label Label_screen;
+
+    @FXML
+    void On_clear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_dive(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_eight(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_equal(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_five(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_four(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_minus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_mutiple(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_nign(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_one(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_plus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_point(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_seven(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_six(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_three(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_two(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_zero(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert Label_screen != null : "fx:id=\"Label_screen\" was not injected: check your FXML file 'View.fxml'.";
+
+    }
+
+}
